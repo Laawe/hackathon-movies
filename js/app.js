31da216cfb138d6d2d3fbcde8c0a4c82e4e23213
Laawe <laura.render.03@gmail.com>
2018-02-07 13:03:09 -0600

    creando archivo js comentarios

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 
-$(document).ready(function() {
-  //funcionalidad Materialize
+$(document).ready(function () {
+    //funcionalidad Materialize
 
-  // funcion para desplegar menú
-  $(".button-collapse").sideNav();
+    // funcion para desplegar menú
+    $(".button-collapse").sideNav();
 
-  //función del carrusel
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
+    //función del carrusel
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
 
- $('#movie-container').hide();
+    $('#movie-container').hide();
 });
 var $search = $('#search').val();
 var $apiURL = "http://www.omdbapi.com/?t=";
@@ -17,8 +17,8 @@ var $apiKey = '&apikey=7da64696';
 var $searcBtn = $('#search-btn');
 //función para llamar al api
 
-$('#search-btn').on('click',function(event){
-  event.preventDefault;
-  var $api = $apiURL + $userInput + $apiKey;
-  console.log($api);
+$('#search-btn').on('click', function (event) {
+    event.preventDefault;
+    var $api = $apiURL + $userInput + $apiKey;
+    console.log($api);
 });
