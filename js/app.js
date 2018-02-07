3aa27bf39f532979253d2674d6785c217ba8eab2
Laawe <laura.render.03@gmail.com>
2018-02-06 20:15:00 -0600

    primer commit, creando repo

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//funcionalidad Materialize
+
+// funcion para desplegar menú
+$(".button-collapse").sideNav();
+
+//función del carrusel
+$('.carousel.carousel-slider').carousel({fullWidth: true});
