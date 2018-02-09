2d7da392bacda7aa00b0bd44e5599af75c7c6a51
Laawe <laura.render.03@gmail.com>
2018-02-09 10:12:30 -0600

    último

--- js/gifs.js
+++ js/gifs.js
@@ -11,26 +11,23 @@ var endPoint = "&limit=3&offset=0&rating=G&lang=es";
 $('#gif').on('click', function (e) {
     e.preventDefault;
     //api
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function (e) {
-        if (this.readyState === 4 && this.status === 200) {
-            var response = JSON.parse(this.response);
-            var gifGif = response.data[0].images.original.url;
-            console.log(gifGif);
+    var xhr = $.get("https://api.giphy.com/v1/gifs/search?" + "api_key=ttTnr3P7XWO1cla8ITffaiE5nfKBjkFu&q=" + document.getElementById("input-gif").value + "&limit=3&offset=0&rating=G&lang=es");
+    xhr.done(function (data) {
+        var containergifs = document.getElementById("container-gifs");
+        console.log("success got data", data);
+        var gif = data.data[0].images.original.url;
+        console.log(gif);
 
-            //
+        var divContainerGif = document.createElement("div");
+        var imagenGif = document.createElement("img");
 
-            var $divContainerGif = $("<div />", {
-                "class": "col s9 offset-s3 container-gif-post"
-            });
-            var $imagenGif = $("<img />", {
-                "src": "gifGif"
-            });
-            //
-            $divContainerGif.append($imagenGif);
-            $("#container-gifs").append($divContainerGif);
-        }
-    }
-    request.open("GET", "https://api.giphy.com/v1/gifs/search?" + "api_key=ttTnr3P7XWO1cla8ITffaiE5nfKBjkFu&q=" + document.getElementById("input-gif").value + "&limit=3&offset=0&rating=G&lang=es", true);
-    request.send();
+        divContainerGif.classList.add("container-gif-post");
+        imagenGif.setAttribute('src', data.data[0].images.original.url);
+        
+        console.log(containergifs, divContainerGif); 
+        divContainerGif.appendChild(imagenGif);
+        containergifs.appendChild(divContainerGif);      
+        //
+
+    });
 });
